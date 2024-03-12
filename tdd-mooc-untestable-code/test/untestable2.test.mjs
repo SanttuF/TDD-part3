a3a651e9094f0b3fc13936f77d47498f41038d02
SanttuF <santtu.f@gmail.com>
2024-03-13 01:43:03 +0200

    tests passed

--- tdd-mooc-untestable-code/test/untestable2.test.mjs
+++ tdd-mooc-untestable-code/test/untestable2.test.mjs
@@ -3,8 +3,19 @@ import { expect } from "chai";
 import { diceHandValue } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+  test("test same fixed roll", () => {
+    expect(diceHandValue(() => 3, () => 3)).to.equal(103)
   });
+  test('test different fixed roll', () => {
+    expect(diceHandValue(() => 3, () => 5)).to.equal(5)
+  })
+  test('test random rolls', () => {
+    const s = new Set()
+
+    for(let i = 0; i < 500; i++) {
+      s.add(diceHandValue())
+    }
+
+    expect(s.size).to.equal(11)
+  })
 });
