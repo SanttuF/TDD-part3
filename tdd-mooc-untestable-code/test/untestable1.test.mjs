faa8b19cc076224bc5e1def8722e15c594565f54
SanttuF <santtu.f@gmail.com>
2024-03-12 04:43:49 +0200

    test passed

--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -3,8 +3,9 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
+  test("counts correctly", () => {
     expect(daysUntilChristmas()).to.be.a("number");
+    expect(daysUntilChristmas(new Date(1880, 11, 24))).to.equal(1)
+    expect(daysUntilChristmas(new Date(1880, 11, 26))).to.equal(364)
   });
 });
