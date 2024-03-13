24972622c4dabeb2f49d71299c467661d1984137
SanttuF <santtu.f@gmail.com>
2024-03-13 04:57:58 +0200

    tests passed

--- tdd-mooc-untestable-code/test/untestable3.test.mjs
+++ tdd-mooc-untestable-code/test/untestable3.test.mjs
@@ -1,13 +1,26 @@
-import { describe, test } from "vitest";
+import { afterEach, beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { parsePeopleCsv } from "../src/untestable3.mjs";
+import { writeFile, unlink } from "node:fs/promises";
 
-// example input:
-// Loid,Forger,,Male
-// Anya,Forger,6,Female
-// Yor,Forger,27,Female
+const input = `
+Loid,Forger,,Male
+Anya,Forger,6,Female
+Yor,Forger,27,Female
+`
 
 describe("Untestable 3: CSV file parsing", () => {
+  beforeEach(() => {
+    try {
+      writeFile('./test.csv', input)
+    } catch (e) {}
+  })
+  afterEach(() => {
+    try {
+      unlink('./test.csv')
+    } catch (e) {}
+  })
+  
   test("todo", async () => {
     // TODO: write proper tests
     try {
